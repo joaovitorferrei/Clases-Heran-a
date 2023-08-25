91793720df17745e2d4ba12f8e0907ea5c1e99e4
joaovitorferrei <84345484viktor@gmail.com>
2023-08-24 21:55:08 -0300

    exxpreçoes regulares

--- class/senha.py
+++ class/senha.py
@@ -1,20 +1,33 @@
 import re
 
 class Usuario:
-    def __init__(self, senha, padrao) -> None:
+    def __init__(self, senha) -> None:
         self.senha = senha 
-        self.padrao = padrao
+        self.padrao = r'^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{8,15}$'
+        self.senha_cadastrada = None
     
-    def validador_senha(self):
+    def cadastro_senha(self):
         if re.match(self.padrao, self.senha):
+            self.senha_cadastrada = self.senha
+            return True
+        else:
+            return False
+    
+    def verificacao_senha(self, senha):
+        if self.senha_cadastrada == senha:
             return True
         else:
             return False
 
-padrao = r'^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{8,15}$'
-s1 = input("Digite a senha: ")
-usuario1 = Usuario(s1, padrao)
-if usuario1.validador_senha():
-    print("Senha válida!")
+s1 = input("Digite a senha para cadastro: ")
+usuario1 = Usuario(s1)
+if usuario1.cadastro_senha():
+    print("Senha cadastrada com sucesso!")
 else:
-    print("Senha inválida.")
+    print("Senha não atende aos requisitos de cadastro.")
+
+s2 = input("Digite sua senha para verificação: ")
+if usuario1.verificacao_senha(s2):
+    print("Senha válida. Parabéns!")
+else:
+    print("Desculpe, senha não corresponde a nenhum cadastro.")
